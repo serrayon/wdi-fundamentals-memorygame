97afdb0c5df680dd13ac2ecc97a8944243f68991
ModernTimes <moderntimes@moderntsmacbook.attlocal.net>
2019-05-06 16:38:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-
-
 var cards = ["queen", "queen", "king", "king"];
-
-
 var cardsInPlay = [];
 
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+	console.log("You found a match!");
+} 
+	else {
+	console.log("Sorry, try again.");
+ }
 
-var cardOne = cards [2];
-var cardTwo = cards [3];
-
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log(`User flipped ${cardOne}.`);
-console.log(`User flipped ${cardTwo}.`);
-
-if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay [1]) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, you lost.');
 }
 
+function flipCard(cardId) {
+	
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
 
+	if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay [1]) {
+	alert('You found a match!');
+	checkForMatch();
+} 
 
+	else {
+	alert('Sorry, you lost.');
+
+ }
+		
+};
+
+flipCard(0);
+flipCard(2);
+checkForMatch(0,3);
