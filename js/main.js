f123fe07b32f504d997d218a4b3fcd5aabb037a8
ModernTimes <moderntimes@ModernTimess-MacBook.local>
2019-05-08 17:28:22 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,21 +34,23 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 	
-	console.log(`User flipped ${cards[cardId]}`);
-	cardsInPlay.push(cards[cardId]);
+	console.log(`User flipped ${cards[cardId.rank]}`);
+	cardsInPlay.push(cards[cardId.rank]);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay [1]) {
 	alert('You found a match!');
 	checkForMatch();
+
+	
 } 
-
 	else {
-	alert('Sorry, you lost.');
-
- }
-		
+		alert('Sorry, you lost.');
+}
 };
+
 
 flipCard(0);
 flipCard(2);
-checkForMatch(0,3);
+checkForMatch();
