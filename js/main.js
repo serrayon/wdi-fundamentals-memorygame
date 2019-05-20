01c19bfb2158a1a328fd1c7f8807532e06cfac1f
ModernTimes <moderntimes@moderntsmacbook.attlocal.net>
2019-05-20 12:40:45 -0700

    cleaned up code added comments

--- js/main.js
+++ js/main.js
@@ -22,17 +22,20 @@ cardImage: "images/king-of-diamonds.png",
 ];
 var cardsInPlay = [];
 
-
-
+//steps to check for match
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay [1]) {
-	console.log("You found a match!");
-} 
-	else {
-	console.log("Sorry, try again.");
- }
+	if (cardsInPlay[0] === cardsInPlay [1]){
+		console.log('You found a match!');
+	}	else{
+		console.log('Sorry, try again.');
+	}
 };
 
+//getAttribute retrieves the data-id of card flipped
+//console.log statement declaraing what card was flipped over
+//push adds the card flipped to the cardsInPlay array
+//2nd and 3rd console.log lets you see the image and suit
+//setAttribute displays the front of the card
 function flipCard() {
 	var cardId = this.getAttribute('data-id');
 	console.log(`User flipped ${cards[cardId].rank}`);
@@ -40,15 +43,25 @@ function flipCard() {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	this.setAttribute('src', cards[cardId].cardImage);
-	if (cardsInPlay.length === 2 && cardsInPlay [0] === cardsInPlay [1]) {
-	alert("You found a match!");
-	checkForMatch();
-} 
-	else {
-		alert("Sorry, you lost.");
- }
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+		if (cardsInPlay[0] === cardsInPlay[1])
+			alert('You found a match!');
+		
+		else{
+			alert('Sorry, try again.');
+			
+		}
+	
+}
 };
 
+//for loop runs through cards array
+//img created and stored in cardElement
+//setAttribute src to see back of card
+//setAttribute data-id to track which card it is
+//eventListener when card is clicked flipCard runs
+//appendchild adds cardElement to game-board
 function createBoard() {
 	for (var i= 0; i< cards.length; i ++) {
 		var cardElement =document.createElement('img');
@@ -58,10 +71,11 @@ function createBoard() {
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 };
+
 function myButton() {
 	location.reload();
-}
+};
 
-createBoard();
 
 checkForMatch();
+createBoard();
